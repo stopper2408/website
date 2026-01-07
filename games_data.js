390675d211c4f77069ad44ab7b791ae9e9229ac8
stopper2408 <219104212+stopper2408@users.noreply.github.com>
2026-01-07 19:21:10 +0000

    Fixes hopefully some issues with games loading

---
+++ games_data.js
@@ -0,0 +1,290 @@
+window.GAMES_DATA = [
+    {
+        "id": "subway-surfers",
+        "title": "Subway Surfers",
+        "image": "thumbnails/subway-surfers.png",
+        "url": "archives/subway-surfers/index.html",
+        "description": "Subway Surfers is a popular mobile game that challenges players to run as fast as they can on subway tracks while avoiding obstacles and collecting coins.",
+        "category": "Action",
+        "popularity": 100
+    },
+    {
+        "id": "slope",
+        "title": "Slope Game",
+        "image": "thumbnails/slope.png",
+        "url": "archives/slope/index.html",
+        "description": "Play Slope Game now!",
+        "category": "Action",
+        "popularity": 95
+    },
+    {
+        "id": "minecraft",
+        "title": "Minecraft (Clone)",
+        "image": "thumbnails/minecraft.png",
+        "url": "archives/minecraft/index.html",
+        "description": "Play Minecraft (Clone) now!",
+        "category": "Arcade",
+        "popularity": 90
+    },
+    {
+        "id": "geometrydash",
+        "title": "Geometry Dash",
+        "image": "thumbnails/geometrydash.png",
+        "url": "archives/geometrydash/index.html",
+        "description": "Play Geometry Dash now!",
+        "category": "Action",
+        "popularity": 85
+    },
+    {
+        "id": "2048",
+        "title": "2048",
+        "image": "thumbnails/2048.png",
+        "url": "archives/2048/index.html",
+        "description": "Play 2048 now!",
+        "category": "Puzzle",
+        "popularity": 80
+    },
+    {
+        "id": "flappybird",
+        "title": "Flappy Bird",
+        "image": "thumbnails/flappybird.png",
+        "url": "archives/flappybird/index.html",
+        "description": "Play Flappy Bird now!",
+        "category": "Arcade",
+        "popularity": 75
+    },
+    {
+        "id": "tetris",
+        "title": "Tetris",
+        "image": "thumbnails/tetris.png",
+        "url": "archives/tetris/index.html",
+        "description": "Play Tetris now!",
+        "category": "Puzzle",
+        "popularity": 70
+    },
+    {
+        "id": "pacman",
+        "title": "Pac-Man",
+        "image": "thumbnails/pacman.png",
+        "url": "archives/pacman/index.html",
+        "description": "Play Pac-Man now!",
+        "category": "Arcade",
+        "popularity": 65
+    },
+    {
+        "id": "cookieclicker",
+        "title": "Cookie Clicker",
+        "image": "thumbnails/cookieclicker.png",
+        "url": "archives/cookieclicker/index.html",
+        "description": "Play Cookie Clicker now!",
+        "category": "Strategy",
+        "popularity": 60
+    },
+    {
+        "id": "run3",
+        "title": "Run",
+        "image": "thumbnails/run3.png",
+        "url": "archives/run3/index.html",
+        "description": "Play Run now!",
+        "category": "Action",
+        "popularity": 55
+    },
+    {
+        "id": "chromedino",
+        "title": "Dino Run",
+        "image": "thumbnails/chromedino.png",
+        "url": "archives/chromedino/index.html",
+        "description": "Play Dino Run now!",
+        "category": "Action",
+        "popularity": 50
+    },
+    {
+        "id": "asteroids",
+        "title": "Asteroids",
+        "image": "thumbnails/asteroids.png",
+        "url": "archives/asteroids/index.html",
+        "description": "Play Asteroids now!",
+        "category": "Action",
+        "popularity": 45
+    },
+    {
+        "id": "snake",
+        "title": "Snake",
+        "image": "thumbnails/snake.png",
+        "url": "archives/snake/index.html",
+        "description": "Play Snake now!",
+        "category": "Arcade",
+        "popularity": 40
+    },
+    {
+        "id": "hextris",
+        "title": "Hextris",
+        "image": "thumbnails/hextris.png",
+        "url": "archives/hextris/index.html",
+        "description": "An addictive puzzle game inspired by Tetris.",
+        "category": "Puzzle",
+        "popularity": 35
+    },
+    {
+        "id": "breakout",
+        "title": "Breakout",
+        "image": "thumbnails/breakout.png",
+        "url": "archives/breakout/index.html",
+        "description": "Play Breakout now!",
+        "category": "Arcade",
+        "popularity": 30
+    },
+    {
+        "id": "chess",
+        "title": "Chess",
+        "image": "thumbnails/chess.png",
+        "url": "archives/chess/index.html",
+        "description": "Play Chess now!",
+        "category": "Arcade",
+        "popularity": 25
+    },
+    {
+        "id": "adarkroom",
+        "title": "A Dark Room",
+        "image": "thumbnails/adarkroom.png",
+        "url": "archives/adarkroom/index.html",
+        "description": "Play A Dark Room now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "blackholesquare",
+        "title": "Black Hole Square",
+        "image": "thumbnails/blackholesquare.png",
+        "url": "archives/blackholesquare/index.html",
+        "description": "Clean the universe!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "bounceback",
+        "title": "Bounce Back",
+        "image": "thumbnails/bounceback.png",
+        "url": "archives/bounceback/index.html",
+        "description": "Play Bounce Back now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "breaklock",
+        "title": "BreakLock",
+        "image": "thumbnails/breaklock.png",
+        "url": "archives/breaklock/index.html",
+        "description": "Play BreakLock now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "captaincallisto",
+        "title": "Captain Callisto",
+        "image": "thumbnails/captaincallisto.png",
+        "url": "archives/captaincallisto/index.html",
+        "description": "Play Captain Callisto now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "chromaincident",
+        "title": "JS13k",
+        "image": "thumbnails/chromaincident.png",
+        "url": "archives/chromaincident/index.html",
+        "description": "Play JS13k now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "connect3",
+        "title": "Connect",
+        "image": "thumbnails/connect3.png",
+        "url": "archives/connect3/index.html",
+        "description": "Play Connect now!",
+        "category": "Puzzle",
+        "popularity": 10
+    },
+    {
+        "id": "evilglitch",
+        "title": "Evil Glitch",
+        "image": "thumbnails/evilglitch.png",
+        "url": "archives/evilglitch/index.html",
+        "description": "Play Evil Glitch now!",
+        "category": "Action",
+        "popularity": 10
+    },
+    {
+        "id": "factoryballsforever",
+        "title": "Factory Balls forever",
+        "image": "thumbnails/factoryballsforever.png",
+        "url": "archives/factoryballsforever/index.html",
+        "description": "Play Factory Balls forever now!",
+        "category": "Puzzle",
+        "popularity": 10
+    },
+    {
+        "id": "packabunchas",
+        "title": "PACKABUNCHAS",
+        "image": "thumbnails/packabunchas.png",
+        "url": "archives/packabunchas/index.html",
+        "description": "Play PACKABUNCHAS now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "particleclicker",
+        "title": "Particle Clicker",
+        "image": "thumbnails/particleclicker.png",
+        "url": "archives/particleclicker/index.html",
+        "description": "An addictive incremental game that was made during the CERN Webfest 2014. It takes the player on a historic journey through modern particle physics.",
+        "category": "Strategy",
+        "popularity": 10
+    },
+    {
+        "id": "racer",
+        "title": "Racer",
+        "image": "thumbnails/racer.png",
+        "url": "archives/racer/index.html",
+        "description": "Play Racer now!",
+        "category": "Sports",
+        "popularity": 10
+    },
+    {
+        "id": "radiusraid",
+        "title": "Radius Raid",
+        "image": "thumbnails/radiusraid.png",
+        "url": "archives/radiusraid/index.html",
+        "description": "Play Radius Raid now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "spacecompany",
+        "title": "Space Company",
+        "image": "thumbnails/spacecompany.png",
+        "url": "archives/spacecompany/index.html",
+        "description": "Play Space Company now!",
+        "category": "Arcade",
+        "popularity": 10
+    },
+    {
+        "id": "trimps",
+        "title": "Trimps",
+        "image": "thumbnails/trimps.png",
+        "url": "archives/trimps/index.html",
+        "description": "Trimps",
+        "category": "Strategy",
+        "popularity": 10
+    },
+    {
+        "id": "underrun",
+        "title": "UNDERRUN",
+        "image": "thumbnails/underrun.png",
+        "url": "archives/underrun/index.html",
+        "description": "Play UNDERRUN now!",
+        "category": "Action",
+        "popularity": 10
+    }
+];
